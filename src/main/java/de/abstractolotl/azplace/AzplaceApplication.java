f5a22e3c69f315ee0c482734f234f280a533d910
NouCake <KevinFischer2010@gmail.com>
2022-07-26 23:15:34 +0200

    removed unused code

--- src/main/java/de/abstractolotl/azplace/AzplaceApplication.java
+++ src/main/java/de/abstractolotl/azplace/AzplaceApplication.java
@@ -7,23 +7,6 @@ import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 public class AzplaceApplication {
-
-    /*
-    @Bean
-    JedisConnectionFactory jedisConnectionFactory() {
-        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration("192.168.1.242", 6379);
-        //redisStandaloneConfiguration.setPassword(RedisPassword.of("yourRedisPasswordIfAny"));
-        return new JedisConnectionFactory(redisStandaloneConfiguration);
-    }
-
-    @Bean
-    public RedisTemplate<String, String> redisTemplate() {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
-    }
-    */
-
     @Bean
     public Jedis jedis() {
         Jedis jedis = new Jedis("192.168.1.242", 6379);
