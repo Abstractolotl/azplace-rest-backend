060ed9ca31fbad05db19640d23f52410dca9b424
Jonas Bender <jbender@united-internet.de>
2022-07-26 11:24:57 +0200

    Initial commit

---
+++ src/main/java/de/abstractolotl/azplace/AzplaceApplication.java
@@ -0,0 +1,13 @@
+package de.abstractolotl.azplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzplaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AzplaceApplication.class, args);
+    }
+
+}
