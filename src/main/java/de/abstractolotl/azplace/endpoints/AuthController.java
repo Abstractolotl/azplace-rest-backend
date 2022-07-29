14e0881fc66f4be19c2b5746b7b1a4a0c6b5a930
Jonas Bender <jbender@united-internet.de>
2022-07-29 08:53:50 +0200

    Changed constructor injection to field injection

--- src/main/java/de/abstractolotl/azplace/endpoints/AuthController.java
+++ src/main/java/de/abstractolotl/azplace/endpoints/AuthController.java
@@ -12,43 +12,34 @@ import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
 import de.abstractolotl.azplace.model.Session;
 import de.abstractolotl.azplace.model.User;
 import de.abstractolotl.azplace.model.UserSession;
 import de.abstractolotl.azplace.repositories.SessionRepo;
 import de.abstractolotl.azplace.repositories.UserRepo;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-
 
 @Component
 public class AuthController implements AuthAPI {
 
-    private final String casUrl;
-    private final String redirectUrl;
-    private final int    defaultKeyValidTime;
-
-    private final UserSession userSession;
-    private final SessionRepo sessionRepo;
-    private final UserRepo    userRepo;
+    @Value("${app.cas.url}")
+    private String casUrl;
+    @Value("${app.cas.redirectUrl}")
+    private String redirectUrl;
+    @Value("${app.defaultKeyValidTime}")
+    private int    defaultKeyValidTime;
 
     @Autowired
-    public AuthController(UserSession userSession,
-                          SessionRepo sessionRepo,
-                          UserRepo    userRepo,
-                          @Value("${app.cas.url}") String casUrl,
-                          @Value("${app.cas.redirectUrl}") String redirectUrl,
-                          @Value("${app.defaultKeyValidTime}") int defaultKeyValidTime) {
-        this.userSession = userSession;
-        this.sessionRepo = sessionRepo;
-        this.userRepo = userRepo;
-        this.casUrl = casUrl;
-        this.defaultKeyValidTime = defaultKeyValidTime;
-        this.redirectUrl = redirectUrl;
-    }
+    private UserSession userSession;
+    @Autowired
+    private SessionRepo sessionRepo;
+    @Autowired
+    private UserRepo    userRepo;
 
     /**
      * Get ticket
@@ -101,28 +92,28 @@ public class AuthController implements AuthAPI {
     }
 
     private boolean isSessionValid() {
-        final Session session = userSession.getSession();
-        final LocalDateTime now = LocalDateTime.now();
+        final Session       session = userSession.getSession();
+        final LocalDateTime now     = LocalDateTime.now();
         if (session.getCreationDate().isAfter(now) || session.getExpireDate().isBefore(now)) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Session key expired");
         }
         return true;
     }
 
-    private String getUserAsString(String response ) {
+    private String getUserAsString(String response) {
         XmlMapper mapper = new XmlMapper();
-        JsonNode authFailureNode;
-        JsonNode authSuccessNode;
+        JsonNode  authFailureNode;
+        JsonNode  authSuccessNode;
         try {
             authFailureNode = mapper.readValue(response, ObjectNode.class).get("authenticationFailure");
             authSuccessNode = mapper.readValue(response, ObjectNode.class).get("authenticationSuccess");
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-        if(authFailureNode != null) {
+        if (authFailureNode != null) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "AuthenticationFailure: " + authFailureNode.get("code"));
         }
-        if(authSuccessNode != null) {
+        if (authSuccessNode != null) {
             if (authSuccessNode.has("user")) {
                 return authSuccessNode.get("user").textValue();
             }
@@ -133,7 +124,7 @@ public class AuthController implements AuthAPI {
     private User getUser(String userString) {
         final List<User> allByInsideNetIdentifier = userRepo.findAllByInsideNetIdentifier(userString);
         if (allByInsideNetIdentifier.isEmpty()) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No user \"" +  userString + "\" found ");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No user \"" + userString + "\" found ");
         }
         return allByInsideNetIdentifier.get(0);
     }
