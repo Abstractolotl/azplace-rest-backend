e7199705def9708e482dffd97340b31e110da91c
dseydel <dseydel@united-internet.de>
2022-07-29 12:53:15 +0200

    Add role to user database

--- src/main/java/de/abstractolotl/azplace/model/User.java
+++ src/main/java/de/abstractolotl/azplace/model/User.java
@@ -15,7 +15,10 @@ public class User {
     private int id;
     private String fullname;
     private String insideNetIdentifier; //TODO: how is a inside net user identified? what does the CAS return.
-    private long timestampLastPixel; //TODO: Discuss how the timestamp should be saved / handeld
+
+    private String role = "default";
+
+    private long timestampLastPixel; //TODO: Discuss how the timestamp should be saved / handled
     private long timestampRegistered;
 
 }
