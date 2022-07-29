592d1bf8b71d219bc3e1eacf334ef118ef06c8bf
Jonas Bender <jbender@united-internet.de>
2022-07-29 09:36:21 +0200

    Put url and port to yml file

--- src/main/java/de/abstractolotl/azplace/AzplaceApplication.java
+++ src/main/java/de/abstractolotl/azplace/AzplaceApplication.java
@@ -1,15 +1,23 @@
 package de.abstractolotl.azplace;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
 import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
 public class AzplaceApplication {
+
+    @Value("${app.redis.url}")
+    private String redisUrl;
+    @Value("${app.redis.port}")
+    private int redisPort;
+
     @Bean
     public Jedis jedis() {
-        Jedis jedis = new Jedis("noucake.ddns.net", 6379);
+        Jedis jedis = new Jedis(redisUrl, redisPort);
         return jedis;
     }
 
