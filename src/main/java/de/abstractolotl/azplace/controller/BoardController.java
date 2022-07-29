cf4b3979b1613771cdbd124f55e53b6b8e0e2678
Jonas Bender <jbender@united-internet.de>
2022-07-29 21:33:20 +0200

    Refactored BoardController and added isSessionValid check at place endpoint

--- src/main/java/de/abstractolotl/azplace/controller/BoardController.java
+++ src/main/java/de/abstractolotl/azplace/controller/BoardController.java
@@ -1,89 +1,83 @@
 package de.abstractolotl.azplace.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.abstractolotl.azplace.AzPlaceExceptions.CanvasNotFoundExeption;
+import de.abstractolotl.azplace.AzPlaceExceptions.IllegalPixelCoordsException;
+import de.abstractolotl.azplace.AzPlaceExceptions.NoUserInSession;
+import de.abstractolotl.azplace.AzPlaceExceptions.PixelOutOfBoundsException;
 import de.abstractolotl.azplace.model.Canvas;
 import de.abstractolotl.azplace.model.PixelOwner;
 import de.abstractolotl.azplace.model.User;
 import de.abstractolotl.azplace.model.requests.PlaceRequest;
 import de.abstractolotl.azplace.repositories.CanvasRepo;
 import de.abstractolotl.azplace.repositories.PixelOwnerRepo;
-import io.swagger.v3.oas.annotations.Operation;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import de.abstractolotl.azplace.AzPlaceExceptions.*;
 import redis.clients.jedis.Jedis;
 
 @RestController
-@RequestMapping("/api/board")
-public class BoardController {
+@RequestMapping("/board")
+public class BoardController implements BoardAPI {
 
-    @Autowired private Jedis jedis;
-    @Autowired private CanvasRepo canvasRepo;
-    @Autowired private PixelOwnerRepo pixelOwnerRepo;
+    @Autowired
+    private Jedis          jedis;
+    @Autowired
+    private CanvasRepo     canvasRepo;
+    @Autowired
+    private PixelOwnerRepo pixelOwnerRepo;
+    @Autowired
+    private AuthController authController;
 
-    @Operation(
-            summary = "Place a Pixel",
-            description = "Placed a pixel on the specified Board." +
-                    "This endpoint is ready for multi board support." +
-                    "for now just use the canasId as 0"
-    )
-    @CrossOrigin(origins = {"*"})
-    @PostMapping("{canvasId}/place")
-    private void place(@PathVariable int canvasId, @RequestBody PlaceRequest request) {
+    @Override
+    public void place(int canvasId, PlaceRequest request, String sessionKey) {
+        authController.isSessionValid(sessionKey);
         checkPixelCords(request.getX(), request.getY());
-        final User user = getUserFromSession();
-        if(user == null) throw new NoUserInSession();
+        final User user = getUserFromSession(sessionKey);
+        if (user == null) {
+            throw new NoUserInSession();
+        }
 
         var canvasResp = canvasRepo.findById(canvasId);
-        if(canvasResp.isEmpty()) throw  new CanvasNotFoundExeption(canvasId);
+        if (canvasResp.isEmpty()) {
+            throw new CanvasNotFoundExeption(canvasId);
+        }
 
         final Canvas canvas = canvasResp.get();
-        if(canvas.getWidth() <= request.getX() || canvas.getHeight() <= request.getY())
+        if (canvas.getWidth() <= request.getX() || canvas.getHeight() <= request.getY()) {
             throw new PixelOutOfBoundsException(request.getX(), request.getY(), canvas.getWidth(), canvas.getHeight());
+        }
 
-        setNewPixelOwner(canvas, request.getX(), request.getY(), getUserFromSession());
+        setNewPixelOwner(canvas, request.getX(), request.getY(), getUserFromSession(sessionKey));
         setPixelInBlob(canvas, request.getX(), request.getY(), request.getColor());
     }
 
-    @Operation(
-            summary = "Board Data",
-            description = "Returns the raw binary data of the board. \n" +
-                    "See the outdated presentation for further information: \n" +
-                    "https://discord.com/channels/@me/758720519804682248/1001125420055400589"
-    )
-    @CrossOrigin(origins = {"*"})
-    @GetMapping("/{canvasId}/data")
-    public byte[] boardData(@PathVariable int canvasId) {
+    @Override
+    public byte[] boardData(int canvasId) {
         var canvasRsp = canvasRepo.findById(canvasId);
-        if(canvasRsp.isEmpty()) throw new CanvasNotFoundExeption(canvasId);
+        if (canvasRsp.isEmpty()) {
+            throw new CanvasNotFoundExeption(canvasId);
+        }
 
         final Canvas canvas = canvasRsp.get();
         return jedis.get(canvas.getRedisKey().getBytes());
     }
 
-    @Operation(
-            summary = "Board Info",
-            description = "Returns the meta information about a Board."
-    )
-    @CrossOrigin(origins = {"*"})
-    @GetMapping("/{canvasId}/info")
-    public Canvas boardInfo(@PathVariable int canvasId) {
+    @Override
+    public Canvas boardInfo(int canvasId) {
         var canvasRsp = canvasRepo.findById(canvasId);
-        if(canvasRsp.isEmpty()) throw new CanvasNotFoundExeption(canvasId);
+        if (canvasRsp.isEmpty()) {
+            throw new CanvasNotFoundExeption(canvasId);
+        }
 
         final Canvas canvas = canvasRsp.get();
         return canvas;
     }
 
-    private User getUserFromSession() {
-        //TODO
-        User user = new User();
-        user.setId(999);
-        user.setFullname("Bobb Bobert");
-        user.setInsideNetIdentifier("nope");
-        user.setTimestampRegistered(System.currentTimeMillis());
-        user.setTimestampLastPixel(System.currentTimeMillis());
-        return user;
+    private User getUserFromSession(String sessionKey) {
+        return authController.getSession(sessionKey).getUser();
     }
 
     private void setPixelInBlob(Canvas canvas, int x, int y, byte color) {
@@ -92,11 +86,15 @@ public class BoardController {
         jedis.bitfield(canvas.getRedisKey(), "SET", "u8", String.valueOf(offset), String.valueOf(color));
     }
 
-    private void checkPixelCords(int x, int y){
-        if(x < 0 || y < 0) throw new IllegalPixelCoordsException();
+    private void checkPixelCords(int x, int y) {
+        if (x < 0 || y < 0) {
+            throw new IllegalPixelCoordsException();
+        }
     }
 
-    /** Notes: Doesn't check for correctnes of coords */
+    /**
+     * Notes: Doesn't check for correctnes of coords
+     */
     private PixelOwner createPixelOwner(Canvas canvas, int x, int y) {
         PixelOwner pixel = new PixelOwner();
         pixel.setCanvas(canvas);
@@ -106,8 +104,8 @@ public class BoardController {
     }
 
     private void setNewPixelOwner(Canvas canvas, int x, int y, User user) {
-        var pixelResp = pixelOwnerRepo.findByXAndY(x, y);
-        PixelOwner pixel = pixelResp.orElseGet(() -> createPixelOwner(canvas, x, y));
+        var        pixelResp = pixelOwnerRepo.findByXAndY(x, y);
+        PixelOwner pixel     = pixelResp.orElseGet(() -> createPixelOwner(canvas, x, y));
         pixel.setTimestamp(System.currentTimeMillis());
         pixel.setUser(user);
         pixelOwnerRepo.save(pixel);
@@ -116,5 +114,4 @@ public class BoardController {
     private int getBlobOffsetForPixel(int width, int height, int x, int y) {
         return y * width + x;
     }
-
 }
--- src/main/java/de/abstractolotl/azplace/model/Canvas.java
+++ src/main/java/de/abstractolotl/azplace/model/Canvas.java
@@ -22,4 +22,3 @@ public class Canvas {
     private int height;
 
 }
-
