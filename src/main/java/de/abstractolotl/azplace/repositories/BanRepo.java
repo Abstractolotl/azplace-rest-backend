e7893f4a91d846216a15dbdb40266ac438c04960
dseydel <dseydel@united-internet.de>
2022-08-01 12:05:36 +0200

    Implement PunishmentService

--- src/main/java/de/abstractolotl/azplace/repositories/BanRepo.java
+++ src/main/java/de/abstractolotl/azplace/repositories/BanRepo.java
@@ -1,8 +1,13 @@
 package de.abstractolotl.azplace.repositories;
 
+import de.abstractolotl.azplace.model.user.User;
 import de.abstractolotl.azplace.model.user.UserBan;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BanRepo extends CrudRepository<UserBan, Long> {
+
+    boolean existsByUserAndPardonedIsFalse(User user);
+
+    UserBan findByUserAndPardonedIsFalse(User user);
 
 }
--- src/main/java/de/abstractolotl/azplace/service/PunishmentService.java
+++ src/main/java/de/abstractolotl/azplace/service/PunishmentService.java
@@ -1,10 +1,30 @@
 package de.abstractolotl.azplace.service;
 
+import de.abstractolotl.azplace.model.user.User;
+import de.abstractolotl.azplace.model.user.UserBan;
+import de.abstractolotl.azplace.repositories.BanRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PunishmentService {
 
-    public 
+    @Autowired
+    private BanRepo banRepo;
+
+    public boolean isBanned(User user){
+        if(!banRepo.existsByUserAndPardonedIsFalse(user))
+            return false;
+
+        UserBan userBan = banRepo.findByUserAndPardonedIsFalse(user);
+
+        if(userBan.getEnd() >= System.currentTimeMillis()){
+            userBan.setPardoned(true);
+            banRepo.save(userBan);
+            return false;
+        }
+
+        return true;
+    }
 
 }
