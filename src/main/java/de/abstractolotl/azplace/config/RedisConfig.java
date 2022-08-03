bd20eccd0dbc6252434b289bca54a52ff340b651
dseydel <dseydel@united-internet.de>
2022-08-03 14:24:46 +0200

    USe spring.redis instead of app.redis

--- src/main/java/de/abstractolotl/azplace/config/RedisConfig.java
+++ src/main/java/de/abstractolotl/azplace/config/RedisConfig.java
@@ -8,11 +8,11 @@ import redis.clients.jedis.Jedis;
 @Configuration
 public class RedisConfig {
 
-    @Value("${app.redis.url}")
+    @Value("${spring.redis.host}")
     private String redisUrl;
-    @Value("${app.redis.port}")
-    private int    redisPort;
-    @Value("${app.redis.password}")
+    @Value("${spring.redis.port}")
+    private int redisPort;
+    @Value("${spring.redis.password}")
     private String redisPassword;
 
     @Bean
@@ -20,6 +20,7 @@ public class RedisConfig {
         if (redisPassword.isBlank()) {
             return new Jedis(redisUrl, redisPort);
         }
+
         final Jedis jedis = new Jedis(redisUrl, redisPort);
         jedis.auth(redisPassword);
         return jedis;
