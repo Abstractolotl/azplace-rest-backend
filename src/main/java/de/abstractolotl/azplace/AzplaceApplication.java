91e4dfbbadc213e4aacd02e5f5cecb4860492d34
dseydel <dseydel@united-internet.de>
2022-08-04 11:46:34 +0200

    Fix merge

--- src/main/java/de/abstractolotl/azplace/AzplaceApplication.java
+++ src/main/java/de/abstractolotl/azplace/AzplaceApplication.java
@@ -2,7 +2,7 @@ package de.abstractolotl.azplace;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication
