87e85e8a388d5326f46be40bf10a1bfafee5456d
dseydel <dseydel@united-internet.de>
2022-08-04 10:43:07 +0200

    Remove CORS Annotation

--- src/main/java/de/abstractolotl/azplace/rest/controller/UserController.java
+++ src/main/java/de/abstractolotl/azplace/rest/controller/UserController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = { "http://localhost:8080" }, allowCredentials = "true")
 public class UserController implements UserAPI {
 
     @Autowired private AuthenticationService authService;
