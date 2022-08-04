6e4db6890e7babd62f37f557724441ae26b29e2b
dseydel <dseydel@united-internet.de>
2022-08-04 16:22:44 +0200

    Add UserSettings to ProfileView

--- src/main/java/de/abstractolotl/azplace/model/user/UserSettings.java
+++ src/main/java/de/abstractolotl/azplace/model/user/UserSettings.java
@@ -1,8 +1,10 @@
 package de.abstractolotl.azplace.model.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class UserSettings {
 
     private boolean anonymize;
--- src/main/java/de/abstractolotl/azplace/model/view/ProfileView.java
+++ src/main/java/de/abstractolotl/azplace/model/view/ProfileView.java
@@ -1,6 +1,7 @@
 package de.abstractolotl.azplace.model.view;
 
 import de.abstractolotl.azplace.model.user.User;
+import de.abstractolotl.azplace.model.user.UserSettings;
 import lombok.Builder;
 import lombok.Data;
 
@@ -11,8 +12,11 @@ public class ProfileView {
     private String name;
     private long timestampRegistered;
 
+    private UserSettings userSettings;
+
     public static ProfileView fromUser(User user) {
         return ProfileView.builder()
+                .userSettings(new UserSettings(user.getRoles().contains("anonymous")))
                 .name(user.getFirstName() + " " + user.getLastName())
                 .timestampRegistered(user.getTimestampRegistered())
                 .build();
