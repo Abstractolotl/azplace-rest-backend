8fad7e89023702c26c053a43509d805ce9e9be99
dseydel <dseydel@united-internet.de>
2022-08-05 16:34:49 +0200

    Fix Punishment API

--- src/main/java/de/abstractolotl/azplace/model/requests/BanRequest.java
+++ src/main/java/de/abstractolotl/azplace/model/requests/BanRequest.java
@@ -3,17 +3,13 @@ package de.abstractolotl.azplace.model.requests;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
-
 @Data
 public class BanRequest {
 
-    @JsonProperty(value = "user_id", required = true)
-    public Integer userId;
+    @JsonProperty("user_id")
+    public int userId;
 
-    @JsonProperty(required = true)
     public String reason;
-
     public long time = -1L;
 
 }
