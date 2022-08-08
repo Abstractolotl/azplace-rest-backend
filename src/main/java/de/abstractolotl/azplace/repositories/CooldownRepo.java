cfd72f744379cf7fdafecc0fcf869773e4514a99
dseydel <dseydel@united-internet.de>
2022-08-08 07:56:55 +0200

    Test Transactional implementation

--- src/main/java/de/abstractolotl/azplace/repositories/CooldownRepo.java
+++ src/main/java/de/abstractolotl/azplace/repositories/CooldownRepo.java
@@ -4,11 +4,14 @@ import de.abstractolotl.azplace.model.board.Canvas;
 import de.abstractolotl.azplace.model.user.User;
 import de.abstractolotl.azplace.model.user.UserCooldown;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
+@Transactional
 public interface CooldownRepo extends CrudRepository<UserCooldown, Long> {
 
+    @Transactional
     Optional<UserCooldown> findByUserAndCanvas(User user, Canvas canvas);
 
 }
