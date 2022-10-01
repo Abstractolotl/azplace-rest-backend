6aeac2a218d12cac6cceb4619a71cf613479ca96
Ancocodet <info@ancozockt.de>
2022-10-01 15:37:28 +0200

    Add session invalidation

--- src/main/java/de/abstractolotl/azplace/rest/api/AuthAPI.java
+++ src/main/java/de/abstractolotl/azplace/rest/api/AuthAPI.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -12,6 +13,11 @@ import org.springframework.web.bind.annotation.*;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.web.servlet.resource.HttpResource;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
 
@@ -64,6 +70,6 @@ public interface AuthAPI {
     })
     @GetMapping(path = "/logout", produces = MediaType.TEXT_HTML_VALUE)
     @ResponseBody
-    void logout(@RequestParam("sessionKey") String sessionKey);
+    ResponseEntity<String> logout(HttpSession httpSession);
 
 }
