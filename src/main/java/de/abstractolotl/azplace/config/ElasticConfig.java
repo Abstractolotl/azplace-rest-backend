c85f282d8ad50f5eb043b6d737951ba424d94dda
dseydel <dseydel@united-internet.de>
2023-05-31 09:46:04 +0200

    Try fixing elastic issue

--- src/main/java/de/abstractolotl/azplace/config/ElasticConfig.java
+++ src/main/java/de/abstractolotl/azplace/config/ElasticConfig.java
@@ -1,76 +1,48 @@
 package de.abstractolotl.azplace.config;
 
-import org.apache.http.HttpHost;
-import org.apache.http.client.config.RequestConfig;
-import org.apache.http.conn.ssl.NoopHostnameVerifier;
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
-import org.apache.http.ssl.SSLContextBuilder;
-import org.apache.http.ssl.SSLContexts;
-import org.elasticsearch.client.RestClient;
-import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
-import java.net.InetSocketAddress;
-import java.security.cert.X509Certificate;
 
 @Configuration
 @ComponentScan
-public class ElasticConfig {
+public class ElasticConfig extends AbstractElasticsearchConfiguration {
 
-    @Value("${spring.elasticsearch.uris}")
-    public String elasticsearchHost;
-    @Value("${spring.elasticsearch.username}")
+    @Value("${elasticsearch.host}")
+    public String elasticsearchUrl;
+    @Value("${elasticsearch.port}")
+    public String elasticsearchPort;
+    @Value("${elasticsearch.username}")
     public String elasticsearchUsername;
-    @Value("${spring.elasticsearch.password}")
+    @Value("${elasticsearch.password}")
     public String elasticsearchPassword;
 
-    @Bean
-    public RestHighLevelClient createSimpleElasticClient() throws Exception {
+    private SSLConfig sslConfig;
+
+    public ElasticConfig() throws Exception {
+        sslConfig = new SSLConfig();
+    }
+
+    @Override
+    public RestHighLevelClient elasticsearchClient() {
+        SSLContext sslContext = null;
         try {
-            SSLContextBuilder sslBuilder = SSLContexts.custom()
-                    .loadTrustMaterial(null, (x509Certificates, s) -> true);
-            final SSLContext sslContext = sslBuilder.build();
-            RestHighLevelClient client = new RestHighLevelClient(RestClient
-                    //port number is given as 443 since its https schema
-                    .builder(new HttpHost(elasticsearchHost, 443, "https"))
-                    .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
-                        @Override
-                        public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
-                            return httpClientBuilder
-                                    .setSSLContext(sslContext)
-                                    .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE);
-                        }
-                    })
-                    .setRequestConfigCallback(new RestClientBuilder.RequestConfigCallback() {
-                        @Override
-                        public RequestConfig.Builder customizeRequestConfig(
-                                RequestConfig.Builder requestConfigBuilder) {
-                            return requestConfigBuilder.setConnectTimeout(5000)
-                                    .setSocketTimeout(120000);
-                        }
-                    }));
-            System.out.println("elasticsearch client created");
-            return client;
+            sslContext = sslConfig.getSSLContext();
         } catch (Exception e) {
-            System.out.println(e);
-            throw new Exception("Could not create an elasticsearch client!!");
+            e.printStackTrace();
         }
-    }
 
-    @Bean
-    public ElasticsearchRestTemplate elasticsearchRestTemplate() throws Exception {
-        return new ElasticsearchRestTemplate(createSimpleElasticClient());
+        final ClientConfiguration config = ClientConfiguration.builder()
+                .connectedTo(elasticsearchUrl + ":" + elasticsearchPort)
+                .usingSsl(sslContext)
+                .withBasicAuth(elasticsearchUsername, elasticsearchPassword)
+                .build();
+        return RestClients.create(config).rest();
     }
-
 }
