ea44c83bab9601e85d589a92fc22fa50588ca2cb
dseydel <dseydel@united-internet.de>
2023-05-31 09:50:43 +0200

    Try fixing elastic issue

--- src/main/java/de/abstractolotl/azplace/config/ElasticConfig.java
+++ src/main/java/de/abstractolotl/azplace/config/ElasticConfig.java
@@ -2,11 +2,13 @@ package de.abstractolotl.azplace.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 
 import javax.net.ssl.SSLContext;
 
@@ -44,5 +46,10 @@ public class ElasticConfig extends AbstractElasticsearchConfiguration {
                 .withBasicAuth(elasticsearchUsername, elasticsearchPassword)
                 .build();
         return RestClients.create(config).rest();
+    }
+
+    @Bean
+    ElasticsearchRestTemplate elasticsearchTemplate() {
+        return new ElasticsearchRestTemplate(elasticsearchClient());
     }
 }
